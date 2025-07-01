50e9283d0b5e383f428ba718992e9bb3be7fa88d
Sami <sami.shoaib0@gmail.com>
2025-07-01 14:40:45 +0500

    initial commit

---
+++ RepositoryView/RepositoryView/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  RepositoryView
+//
+//  Created by Macbook on 03/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            List {
+                RepositoryCardView()
+                    .listRowSeparator(.hidden)
+                RepositoryCardView()
+                    .listRowSeparator(.hidden)
+                RepositoryCardView()
+                    .listRowSeparator(.hidden)
+                RepositoryCardView()
+                    .listRowSeparator(.hidden)
+                RepositoryCardView()
+                    .listRowSeparator(.hidden)
+                RepositoryCardView()
+                    .listRowSeparator(.hidden)
+                
+            }
+            .listStyle(.plain)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct RepositoryCardView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Repository Name:")
+                Text("My Repo")
+            }
+            
+            HStack {
+                Text("Total Stars:")
+                Text("600")
+            }
+            
+            HStack {
+                Text("Language:")
+                Text("Swift")
+            }
+            
+            HStack {
+                Text("Description:")
+                Text("My description")
+            }
+            
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(.systemGray6))
+        )
+    }
+}
---
+++ RepositoryView/RepositoryView/RepositoryViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryViewApp.swift
+//  RepositoryView
+//
+//  Created by Macbook on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RepositoryViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
